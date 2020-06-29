e8add6fa7efc0160b317c1b21d98a26b8764cbbd
Alex Kim <alex.kim@email.cz>
2020-06-29 19:49:26 +0200

    Some of the global moved to classes

--- rockpaperscissors_with_graphics.py
+++ rockpaperscissors_with_graphics.py
@@ -20,10 +20,8 @@ o_scissors_pos = (950, 150)
 players_choice_pos = (650, 200)
 comps_choice_pos = (150, 200)
 questionmark_pos = (50, 250)
-#score information
-round_no = 1 # the round will always begin from number 1
-score = [0, 0]
-click_record = [0, 0]
+
+
 its_a_tie = 0 # if the game is evaluated as "its a tie", the value turns 1
 #image properties
 play_image_size = (265, 215)
@@ -58,70 +56,54 @@ tie_label = text.Label(f"It's a tie!", font_size = 20, x = 550, y = 250, anchor_
 chose_one = text.Label(f"click \non one:", font_size = 20, x = 1050, y = 470, width = 100,
                                 align="center", anchor_x = "right", anchor_y="center", multiline=True)
 
-#score and round information screen from the beginning of the game
-def newgame():
-    global player_score_screen, pc_score_screen, round_screen
-    player_score_screen = text.Label(f"Player's\nscore: {str(score[1])}",
-                                font_size = 30, x = 800, y = 520, width = 300,
-                                align="center", anchor_x = "center", anchor_y="center", multiline=True)
-    pc_score_screen = text.Label(f"Computer's\nscore: {str(score[0])}",
-                                font_size = 30, x = 280, y = 520, width = 300,
-                                align="center", anchor_x = "center", anchor_y="center", multiline=True)
-    round_screen = text.Label(f"round: {round_no}", font_size = 40, x = 550, y = 80, anchor_x="center")
+
 
 @window.event
 def on_draw():
-    global pc_start, player_score_screen, pc_score_screen, score, player_start, round_screen, its_a_tie
+    global pc_start, score, player_start, its_a_tie
     window.clear()
-    newgame()
+    game.draw()
+    game.restart()
+    #human.draw()
+    #pc.draw()
     options_all.draw()
     pc_start.draw()
-    #score
-    player_score_screen.draw()
-    pc_score_screen.draw()
     chose_one.draw()
     if its_a_tie == 1:
          tie_label.draw()
-    #round
-    if round_no > 3: # if the round is higher than 3, stop the number at 3 (dont rise the number to 4)
-        round_screen = text.Label(f"round: 3", font_size = 40, x = 550, y = 80, anchor_x="center")
-        if score[0] > score[1]:
-            gameover_screen = text.Label(f"Computer wins!\nPress space to restart", font_size = 24, x = 550, y = 30, anchor_x = "center").draw()
-        elif score[1] > score[0]:
-            gameover_screen = text.Label(f"You win!\nPress space to restart", font_size = 24, x = 550, y = 30, anchor_x = "center").draw()
-    round_screen.draw()
+    #round_screen.draw()
     player_start.draw()
 
 @window.event
 def on_mouse_press(x, y, b, mod):
-    global click_record, chosen_image, player_start, round_no, score
-    click_record[0] = x
-    click_record[1] = y
-    if round_no < 4:
+    global chosen_image, player_start, round_no, score
+    game.click_record[0] = x
+    game.click_record[1] = y
+    if game.round_no < 4:
     #options change
         if bounding_box(x, y, o_rock_pos, choice_image_size):
             chosen_image = image_player_rock
             player_start = sprite.Sprite(chosen_image, x = 650, y = 200)
-            evaluate(image_player_rock, computer_chose())
+            game.evaluate(image_player_rock, computer_chose())
         elif bounding_box(x, y, o_paper_pos, choice_image_size):
             chosen_image = image_player_paper
             player_start = sprite.Sprite(chosen_image, x = 650, y = 200)
-            evaluate(image_player_paper, computer_chose())
+            game.evaluate(image_player_paper, computer_chose())
         elif bounding_box(x, y, o_scissors_pos, choice_image_size):
             chosen_image = image_player_scissors
             player_start = sprite.Sprite(chosen_image, x = 650, y = 200)
-            evaluate(image_player_scissors, computer_chose())
+            game.evaluate(image_player_scissors, computer_chose())
         else:
             pass # nothing changes if clicked outside the bounding boxes
 
 @window.event
 def on_key_press(symbol, modyfiers):
     global round_no, score
-    if round_no > 3:
+    if game.round_no > 3:
         if symbol == key.SPACE:
-            round_no = 1
-            score = [0, 0]
-            newgame()
+            game.round_no = 1
+            game.score = [0, 0]
+            game.restart()
 
 #create a bounding box area of the image and evaluate if user clicked in the bounding box area
 def bounding_box(v1, v2, v_pos, v_size):
@@ -137,21 +119,55 @@ def computer_chose():
     pc_start = sprite.Sprite(pc_option, x = 150, y = 200)
     return pc_option
 
-#this function takes a player's and computer's choice and based of evaluate adds score to player, computer or nobody
-def evaluate(player, computer):
-    global player_score_screen, pc_score_screen, round_no, round_screen, score, its_a_tie
-        #player wins
-    if player == image_player_rock and computer == image_pc_scissors or player == image_player_scissors and computer == image_pc_paper or player == image_player_paper and computer == image_pc_rock:
-        score[1] = score[1] + 1 #adds score number
-        its_a_tie = 0
-        round_no = round_no + 1 #adds round number
-        #computer wins
-    elif player == image_player_scissors and computer == image_pc_rock or player == image_player_paper and computer == image_pc_scissors or player == image_player_rock and computer == image_pc_paper:
-        score[0] = score[0] + 1
-        its_a_tie = 0
-        round_no = round_no + 1
-        #its a tie
-    elif player == image_player_rock and computer == image_pc_rock or player == image_player_scissors and computer == image_pc_scissors or player == image_player_paper and computer == image_pc_paper:
-        its_a_tie = 1
+class Game():
+    def __init__(self):
+        #score information
+        self.round_no = 1 # the round will always begin from number 1
+        self.score = [0, 0]
+        self.click_record = [0, 0]
 
+    def restart(self):
+        #round
+        if self.round_no > 3: # if the round is higher than 3, stop the number at 3 (dont rise the number to 4)
+            self.round_screen = text.Label(f"round: 3", font_size = 40, x = 550, y = 80, anchor_x="center")
+            if self.score[0] > self.score[1]:
+                self.gameover_screen = text.Label(f"Computer wins!\nPress space to restart", font_size = 24, x = 550, y = 30, anchor_x = "center")
+                self.gameover_screen.draw()
+            elif self.score[1] > self.score[0]:
+                self.gameover_screen = text.Label(f"You win!\nPress space to restart", font_size = 24, x = 550, y = 30, anchor_x = "center")
+                self.gameover_screen.draw()
+    #score and round information screen from the beginning of the game
+    def draw(self):
+        #global player_score_screen, pc_score_screen, round_screen
+        text.Label(f"Player's\nscore: {str(self.score[1])}",
+                                    font_size = 30, x = 800, y = 520, width = 300,
+                                    align="center", anchor_x = "center", anchor_y="center", multiline=True).draw()
+        text.Label(f"Computer's\nscore: {str(self.score[0])}",
+                                    font_size = 30, x = 280, y = 520, width = 300,
+                                    align="center", anchor_x = "center", anchor_y="center", multiline=True).draw()
+        self.round_screen = text.Label(f"round: {self.round_no}", font_size = 40, x = 550, y = 80, anchor_x="center").draw()
+
+    #this function takes a player's and computer's choice and based of evaluate adds score to player, computer or nobody
+    def evaluate(self, player, computer):
+        #global player_score_screen, pc_score_screen, round_no, round_screen, score, its_a_tie
+            #player wins
+        if player == image_player_rock and computer == image_pc_scissors or player == image_player_scissors and computer == image_pc_paper or player == image_player_paper and computer == image_pc_rock:
+            self.score[1] = self.score[1] + 1 #adds score number
+            its_a_tie = 0
+            self.round_no = self.round_no + 1 #adds round number
+            #computer wins
+        elif player == image_player_scissors and computer == image_pc_rock or player == image_player_paper and computer == image_pc_scissors or player == image_player_rock and computer == image_pc_paper:
+            self.score[0] = self.score[0] + 1
+            its_a_tie = 0
+            self.round_no = self.round_no + 1
+            #its a tie
+        elif player == image_player_rock and computer == image_pc_rock or player == image_player_scissors and computer == image_pc_scissors or player == image_player_paper and computer == image_pc_paper:
+            its_a_tie = 1
+
+    #class Player(): - to be created
+
+
+game = Game()
+#human = Player()
+#pc = Player()
 app.run()
